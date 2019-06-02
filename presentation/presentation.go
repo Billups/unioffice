4d031ec2ac621d13502dc6b5f40960107d3e9698
Franklin Harding <frank.harding@billups.com>
2019-06-02 15:56:36 -0700

    Add support for ImageFromBytes to presentation
    Literally copies a snippet from the document library for in-memory
    images to the presentation library

--- presentation/presentation.go
+++ presentation/presentation.go
@@ -442,7 +442,11 @@ func (p *Presentation) Save(w io.Writer) error {
 	for i, img := range p.Images {
 		fn := unioffice.AbsoluteFilename(unioffice.DocTypePresentation, unioffice.ImageType, i+1)
 		fn = fn[0:len(fn)-3] + strings.ToLower(img.Format())
-		if img.Path() != "" {
+		if img.Data() != nil {
+			if err := zippkg.AddFileFromBytes(z, fn, *img.Data()); err != nil {
+				return err
+			}
+		} else if img.Path() != "" {
 			if err := zippkg.AddFileFromDisk(z, fn, img.Path()); err != nil {
 				return err
 			}
